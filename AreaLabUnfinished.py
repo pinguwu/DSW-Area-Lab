3ac1b83bf3d269b06cfe3a1dc41420f950f7d615
pinguwu <premixedcloud977@gmail.com>
2018-09-25 11:13:38 -0700

    added equations

--- AreaLabUnfinished.py
+++ AreaLabUnfinished.py
@@ -1,26 +1,31 @@
-import math 
-import unittest
+import math #lets us use the math module
+import unittest #unittest lets us test small sections of code
 
 def circleArea(radius): 
-    return 1
+    return radius * radius * math.pi
 
 def rectArea(base, height):
-    return 1
+    return base * height
 
 def trapArea(base1, base2, height):
-    return 1
+    return height * (base1 + base2) / 2
 
 def triArea(base,height):
-    return 1
+    return (base * height) / 2
     
 	
-class MyTest(unittest.TestCase):
+class MyTest(unittest.TestCase): #using TestCase class from unittest module
     def testCircleArea(self):
-    
+        self.assertEqual(circleArea(5), 25 * math.pi)
+        self.assertEqual(circleArea(2), 4 * math.pi)
     def testRectArea(self):
-    
+        self.assertEqual(rectArea(2, 2), 2 * 2)
+        self.assertEqual(rectArea(123, 321), 123 * 321)
     def testTrapArea(self):
-    
+        self.assertEqual(trapArea(3, 4, 5), 5 * (3 + 4) / 2)
+        self.assertEqual(trapArea(1, 2, 3), 3 * (1 + 2) / 2)
     def testTriArea(self):
+        self.assertEqual(triArea(1, 3), (1 * 3) / 2)
+        self.assertEqual(triArea(9, 8), (9 * 8) /2)
     
     
